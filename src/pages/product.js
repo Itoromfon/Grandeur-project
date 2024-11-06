bc52cf5e486aca73ff8d19d82b4d414d967d38dd
itoromfon <itoromfon746@gmail.com>
2024-11-07 00:30:30 +0100

    added some file

--- src/pages/product.js
+++ src/pages/product.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '@/components/navbar/Navbar'
 import Carousel from '../components/carousel/Carousel'
 import Carousel2 from '@/components/carousel2/Carousel2'
-import { EmblaCarousel } from '@/components/emblacarousel/EmblaCarousel'
+import EmblaCarousel from '@/components/emblacarousel/EmblaCarousel'
 
 function product() {
   return (
