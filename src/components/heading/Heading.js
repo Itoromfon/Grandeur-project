125184cfb94ec233eb95a42c0c3e547d7fb5b55e
itoromfon <itoromfon746@gmail.com>
2024-11-27 16:33:43 +0100

    made some changes

--- src/components/heading/Heading.js
+++ src/components/heading/Heading.js
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NightMode from '../nightmode/NightMode'
+import styles from './Heading.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+import { GrFormNext } from "react-icons/gr";
 
 function Heading() {
+  // State to track if the button is on or off
+  const [isOn, setIsOn] = useState(false);
+
+  // Toggle function to change the state
+  const toggleButton = () => {
+    setIsOn(!isOn);
+  };
+
   return (
-    <div>Heading</div>
+    <div className={styles.main}>
+        <div>
+            <NightMode />
+        </div>
+        <div className={styles.headdiv}>
+        <div>
+            <h1 className={styles.h1text}>Heading Title will Go here.</h1>
+        </div>
+        <div className={styles.p1textdiv}>
+            <p className={styles.p1text}>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+                sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+                magna aliquam erat volutpat. Ut wisi enim ad minim veniam, 
+                quis nostrud exerci tation ullamcorper suscipit lobortis nisl 
+                ut aliquip ex ea commodo consequat. Duis autem
+            </p>
+        </div>
+        <div>
+            <div>
+                <h1 className={styles.activateh1}>Activate Routine</h1>
+            </div>
+            <div className={styles.maindiv}>
+            <div>
+            <button
+            onClick={toggleButton}
+            className={styles.buttontoggle}
+            >
+            <FontAwesomeIcon
+                icon={isOn ? faToggleOn : faToggleOff} // Show different icons based on state
+                color={isOn ? '#4CAF50' : '#c4c4c6'}  // Green for "on", Red for "off"
+            />
+            </button>
+            </div>
+            <div className={styles.viewdiv}>
+                <div className={styles.viewlinkdiv}>
+                    <Link className={styles.viewlink} href='#'>View All Routine</Link>
+                </div>
+                <div>
+                    <GrFormNext className={styles.nextlink} />
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
   )
 }
 
---
+++ src/components/nightmode/NightMode.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import styles from './NightMode.module.css'
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
+import Image from 'next/image'
+
+function NightMode() {
+  return (
+    <div className={styles.main}>
+        <div className={styles.NightMode}>
+            <div>
+                <h1 className={styles.text}>Night Mode</h1>
+            </div>
+            <div className={styles.moondiv}>
+                <Image 
+                    src='/groupmoon.png'
+                    alt='Moon'
+                    width={47}
+                    height={47}
+                />
+            </div>
+            <div className={styles.bulbdiv}>
+            <div>
+                <Image 
+                    src='/groupbulb.png'
+                    alt='bulb'
+                    width={48}
+                    height={48}
+                />
+            </div>
+            <div>
+                <p className={styles.bulbtext}>Adjusts Lights</p>
+            </div>
+            </div>
+            <div className={styles.healthdiv}>
+                <div className={styles.secureimg}>
+                    <AiOutlineSafetyCertificate className={styles.safetyicon} />
+                </div>
+                <div>
+                    <p className={styles.safetytext}>Health and Safety Alerts</p>
+                </div>
+            </div>
+            <div className={styles.healthdiv2}>
+                <div className={styles.secureimg}>
+                    <AiOutlineSafetyCertificate className={styles.safetyicon2} />
+                </div>
+                <div>
+                    <p className={styles.safetytext2}>Health and Safety Alerts</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NightMode
